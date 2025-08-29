b9b307b503b92b47e40127bff39586cda1856607
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-29 14:25:26 +0200

    add: TaskCreateView and html

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task, Tag
+from .forms import TaskCreateForm
+from django.urls import reverse_lazy
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin,TemplateView):
     template_name = "todo/index.html"
-    
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy('todo/index.html')
+    template_name = "todo/task_create_form.html"
