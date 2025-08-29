ece019627e97e37f3df378271b708594a15dad76
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-29 14:24:57 +0200

    update: gitignore

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
